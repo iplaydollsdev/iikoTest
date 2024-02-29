010619dbb149e8fc6c13b94db85ffb75ec3ea5d9
iplaydollsdev <iplaydollsdev@gmail.com>
2024-02-29 13:29:14 +0300

    Check before sending

--- iikoTestServer/Program.cs
+++ iikoTestServer/Program.cs
@@ -13,32 +13,6 @@ namespace iikoTestServer
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        //static void Main()
-        //{
-        //    ServiceCollection services = new ServiceCollection();
-        //    services.AddHttpClient();
-        //    ServiceProvider serviceProvider = services.BuildServiceProvider();
-
-        //    Application.EnableVisualStyles();
-        //    Application.SetCompatibleTextRenderingDefault(false);
-        //    Application.Run(new MainWindow(serviceProvider.GetRequiredService<IHttpClientFactory>()));
-        //}
-        //static void Main()
-        //{
-        //    Application.EnableVisualStyles();
-        //    Application.SetCompatibleTextRenderingDefault(false);
-
-        //    var services = new ServiceCollection();
-        //    services.AddHttpClient(); // Register IHttpClientFactory in the service collection
-        //    var serviceProvider = services.BuildServiceProvider();
-
-        //    var httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
-
-        //    var mainWindowModel = new MainWindowModel(httpClientFactory);
-        //    var mainWindow = new MainWindow();
-        //    var presenter = new MainPresenter(mainWindow, mainWindowModel);
-        //    Application.Run(mainWindow);
-        //}
         static void Main()
         {
             Application.EnableVisualStyles();
@@ -46,24 +20,19 @@ namespace iikoTestServer
 
             var services = new ServiceCollection();
 
-            // Регистрация служб в контейнере зависимостей
-            services.AddHttpClient(); // Регистрация IHttpClientFactory в контейнере служб
+            services.AddHttpClient();
 
-            // Зарегистрируйте представление и другие службы
-            services.AddScoped<IMainWindowView, MainWindow>(); // Зарегистрируйте интерфейс IMainWindowView с реализацией MainWindow как службу
-            //services.AddScoped<IDataService, DataService>(); // Зарегистрируйте DataService как службу
+            services.AddScoped<IMainWindowView, MainWindow>();
             services.AddSingleton<IDataService, DataService>();
 
             var serviceProvider = services.BuildServiceProvider();
 
-            // Получение необходимых сервисов из контейнера
-            var mainWindow = serviceProvider.GetRequiredService<IMainWindowView>(); // Получите экземпляр представления из контейнера
-            var dataService = serviceProvider.GetRequiredService<IDataService>(); // Получите экземпляр DataService из контейнера
+            var mainWindow = serviceProvider.GetRequiredService<IMainWindowView>();
+            var dataService = serviceProvider.GetRequiredService<IDataService>();
 
-            // Создание и запуск презентера для главного окна
             var presenter = new MainWindowPresenter(mainWindow, dataService);
 
-            Application.Run((mainWindow as Form)); // Запуск главной очереди сообщений приложения
+            Application.Run((mainWindow as Form));
         }
 
     }
